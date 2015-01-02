ad4ed7a152dea98550285ed12ec6a60b2baeaac8
CoderDojoCarmarthen <code@code-carmarthen.co.uk>
2015-01-02 20:23:31 +0000

    Creeper
    A simple program to display a creeper on the display

---
+++ creeper.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import unicornhat as UH
+import time
+
+
+UH.rotation(270)
+
+
+#for y in range(5):
+#  for x in range(5):
+#    UH.set_pixel(x,y,0,255,255)
+#    UH.show()
+#    time.sleep(0.05)
+
+#time.sleep(1)
+
+#row 0
+UH.set_pixel(0,0,0,255,0)
+UH.set_pixel(0,1,0,255,0)
+UH.set_pixel(0,2,0,255,0)
+UH.set_pixel(0,3,0,255,0)
+UH.set_pixel(0,4,0,255,0)
+UH.set_pixel(0,5,0,255,0)
+UH.set_pixel(0,6,0,255,0)
+UH.set_pixel(0,7,0,255,0)
+
+#1
+UH.set_pixel(1,0,0,255,0)
+UH.set_pixel(1,1,0,255,0)
+UH.set_pixel(1,2,0,255,0)
+UH.set_pixel(1,3,0,255,0)
+UH.set_pixel(1,4,0,255,0)
+UH.set_pixel(1,5,0,255,0)
+UH.set_pixel(1,6,0,255,0)
+UH.set_pixel(1,7,0,255,0)
+
+#2
+UH.set_pixel(2,0,0,255,0)
+UH.set_pixel(2,1,0,0,255)
+UH.set_pixel(2,2,0,0,255)
+UH.set_pixel(2,3,0,255,0)
+UH.set_pixel(2,4,0,255,0)
+UH.set_pixel(2,5,0,0,255)
+UH.set_pixel(2,6,0,0,255)
+UH.set_pixel(2,7,0,255,0)
+
+#3
+UH.set_pixel(3,0,0,255,0)
+UH.set_pixel(3,1,0,0,255)
+UH.set_pixel(3,2,0,0,255)
+UH.set_pixel(3,3,0,255,0)
+UH.set_pixel(3,4,0,255,0)
+UH.set_pixel(3,5,0,0,255)
+UH.set_pixel(3,6,0,0,255)
+UH.set_pixel(3,7,0,255,0)
+
+#4
+UH.set_pixel(4,0,0,255,0)
+UH.set_pixel(4,1,0,255,0)
+UH.set_pixel(4,2,0,255,0)
+UH.set_pixel(4,3,0,0,255)
+UH.set_pixel(4,4,0,0,255)
+UH.set_pixel(4,5,0,255,0)
+UH.set_pixel(4,6,0,255,0)
+UH.set_pixel(4,7,0,255,0)
+
+#5
+UH.set_pixel(5,0,0,255,0)
+UH.set_pixel(5,1,0,255,0)
+UH.set_pixel(5,2,0,0,255)
+UH.set_pixel(5,3,0,0,255)
+UH.set_pixel(5,4,0,0,255)
+UH.set_pixel(5,5,0,0,255)
+UH.set_pixel(5,6,0,255,0)
+UH.set_pixel(5,7,0,255,0)
+
+#6
+UH.set_pixel(6,0,0,255,0)
+UH.set_pixel(6,1,0,255,0)
+UH.set_pixel(6,2,0,0,255)
+UH.set_pixel(6,3,0,0,255)
+UH.set_pixel(6,4,0,0,255)
+UH.set_pixel(6,5,0,0,255)
+UH.set_pixel(6,6,0,255,0)
+UH.set_pixel(6,7,0,255,0)
+
+#7
+UH.set_pixel(7,0,0,255,0)
+UH.set_pixel(7,1,0,255,0)
+UH.set_pixel(7,2,0,0,255)
+UH.set_pixel(7,3,0,255,0)
+UH.set_pixel(7,4,0,255,0)
+UH.set_pixel(7,5,0,0,255)
+UH.set_pixel(7,6,0,255,0)
+UH.set_pixel(7,7,0,255,0)
+UH.show()
+
+
+time.sleep(10)
+
